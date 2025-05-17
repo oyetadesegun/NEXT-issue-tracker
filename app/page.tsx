eaacd5fffb1ad59cc0593fcabc7924e7c4c68f82
Oyetade Segun <johnwellacademy@gmail.com>
2025-05-17 18:48:41 +0100

    second

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-
 export default function Home() {
   return (
-   <>
-   <div>Hello World</div>
-   </>
-  )
+    <div className="flex min-h-screen items-center justify-center bg-red-100">
+      <h1 className="text-5xl font-bold text-blue-600 underline">
+        Tailwind IS WORKING!
+      </h1>
+    </div>
+  );
 }
