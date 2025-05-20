5cb7a36c741c5c858c4092b4316db153334a88af
Oyetade Segun <johnwellacademy@gmail.com>
2025-05-20 02:53:45 +0100

    Handle Error

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -2,8 +2,8 @@ import { NextRequest, NextResponse } from "next/server";
 import { z } from 'zod';
 import prisma from "@/prisma/client";
 const createIssueSchema = z.object({
-    title: z.string().min(1).max(255),
-    description: z.string().min(1)
+    title: z.string().min(1, 'Title is required.').max(255),
+    description: z.string().min(1, 'Description is required.')
 });
 
 export async function POST(request: NextRequest) {
@@ -11,7 +11,7 @@ export async function POST(request: NextRequest) {
     const validation = createIssueSchema.safeParse(body);
 
     if (!validation.success) {
-        return NextResponse.json(validation.error.errors, { status: 400 });
+        return NextResponse.json(validation.error.format(), { status: 400 });
     }
     const { title, description } = createIssueSchema.parse(body);
 
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,6 +1,6 @@
 'use client'
-import { Button, TextField } from '@radix-ui/themes';
-import React from 'react';
+import { Button, Callout, TextField } from '@radix-ui/themes';
+import React, { useState } from 'react';
 import { useForm, Controller } from 'react-hook-form'
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
@@ -15,10 +15,20 @@ const NewIssue = () => {
   const router = useRouter();
   const { register, control, handleSubmit } = useForm<IssueForm>();
   console.log(register('title'))
+  const [error, setError] = useState('')
   return (
+    <div className='max-w-xl'>
+     {error && <Callout.Root color="red" className='mb-5'> 
+      <Callout.Text>{error}</Callout.Text>
+      </Callout.Root>}
     <form className='max-w-xl p-5 space-y-3' onSubmit={handleSubmit(async (data) => {
-      await axios.post('/api/issues', data);
+      try {
+         await axios.post('/api/issues', data);
       router.push('/issues');
+      } catch (error) {
+        setError('An unexpected error occurred.')
+      }
+     
     })}>
       <TextField.Root placeholder='Title' {...register('title')} />
       <Controller
@@ -29,6 +39,7 @@ const NewIssue = () => {
 
       <Button>Submit New Issue</Button>
     </form>
+    </div>
   )
 }
 
