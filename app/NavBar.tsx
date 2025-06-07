1a68c258f0a7b97b0b1e70928af633709e1395df
Oyetade Segun <johnwellacademy@gmail.com>
2025-06-07 21:48:47 +0100

    Navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,31 +1,28 @@
-'use client';
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
+import React from 'react'
 import { AiFillBug } from 'react-icons/ai'
-import classnames from 'classnames';
 
 const NavBar = () => {
-    const currentPath = usePathname()
-    const links = [
-        { href: '/', label: 'Dashboard' },
-        { href: '/issues', label: 'Issues' },
-        { href: '/issues/new', label: 'New Issues' },
-    ]
-    return (
-        <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
-            <Link href='/'><AiFillBug /></Link>
-            <ul className='flex space-x-6'>
-                {links.map(link => <Link key={link.href} className={classnames({
-                    'text-blue-500 hover:text-blue-700': currentPath === link.href,
-                    'text-gray-500 hover:text-gray-700': currentPath !== link.href,
-                    'hover:text-zinc-800 transition-colors': true
-                    
-                })} href={link.href}>{link.label}</Link>)}
+  const Links = [
+    {
+      name: 'Dashboard',
+      url: '/',
+    },
+    {
+      name: 'Issues',
+      url: '/issues',
+    }
+  ]
+  return (
+    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
+      <Link href='/'><AiFillBug /></Link>
 
+      <ul className='flex space-x-6 '>
+        {Links.map(link => (<li><Link key={link.url} className='text-zinc-500 hover:text-zinc-800 transition-colors' href={link.url}>{link.name}</Link></li>))}
+      </ul>
 
-            </ul>
-        </nav>
-    )
+    </nav>
+  )
 }
 
 export default NavBar
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
+import Link from "next/link";
 
 
 export default function Home() {
   return (
     <>
-    
-     <p>Hello World!</p>
-    
+    <p>Hello World</p>
     </>
   );
 }
