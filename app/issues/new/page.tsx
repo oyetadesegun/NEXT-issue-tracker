381bce517fc344fd803a304c584335e0cb2a677f
Oyetade Segun <johnwellacademy@gmail.com>
2025-06-09 15:10:55 +0100

    disable ssr markdown

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,6 +1,5 @@
 'use client'
 import { Button, Callout, Spinner, TextField } from '@radix-ui/themes'
-import SimpleMDE from 'react-simplemde-editor'
 import "easymde/dist/easymde.min.css"
 import { useForm, Controller } from 'react-hook-form'
 import axios from 'axios'
@@ -9,7 +8,11 @@ import { useState } from 'react'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { createIssueSchema } from '@/app/validationSchema'
 import { z } from 'zod'
+import dynamic from 'next/dynamic'
 import ErrorMessage from '@/components/ErrorMessage'
+
+const SimpleMDE = dynamic(()=>import('react-simplemde-editor'),
+{ssr: false})
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 const NewIssuePage = () => {
